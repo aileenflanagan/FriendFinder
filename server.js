88d4b6c35cc95dec87fef0b84033e2b11ed2da64
Aileen Flanagan <aileenmflanagan@hotmail.com>
2019-03-04 15:00:33 -0600

    added server to server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 var express=require("express");
 var app= express();
-// var PORT;
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
